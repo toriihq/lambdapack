f6f63d286bd058d16ef0b3f31c25a1b80ace2aa8
Tal Bereznitskey <berzniz@gmail.com>
2019-03-22 15:32:06 +0200

    Use yarn if yarn.lock exists

--- scripts/getProductionDeps.js
+++ scripts/getProductionDeps.js
@@ -1,29 +1,56 @@
+const fs = require('fs')
+const path = require('path')
 const { execSync } = require('child_process')
 
-const flattenDeps = (obj, deps) => {
+const flattenNpmDeps = (obj, deps) => {
   const { dependencies } = (obj || {})
   if (typeof dependencies !== 'object') {
     return
   }
   Object.keys(dependencies).forEach(dep => {
     deps[dep] = true
-    flattenDeps(dependencies[dep], deps)
+    flattenNpmDeps(dependencies[dep], deps)
   })
 }
 
-const readProductionDeps = () => {
+const readProductionDeps = (cmd) => {
   let output
   try {
-    output = execSync('npm ls --prod --json 2>/dev/null')
+    output = execSync(cmd)
   } catch (e) {
     output = e.stdout
   }
   return JSON.parse(output.toString())
 }
 
-module.exports = () => {
+const flattenYarnDeps = (obj, deps) => {
+  const { children } = (obj || {})
+  if (!children || children.length === 0) {
+    return
+  }
+  children.forEach(depObject => {
+    const lastIndex = depObject.name.lastIndexOf('@')
+    const dep = lastIndex >= 0 ? depObject.name.substr(0, lastIndex) : depObject.name
+    deps[dep] = true
+    flattenYarnDeps(depObject, deps)
+  })
+}
+
+const useYarn = () => {
   const deps = {}
-  const json = readProductionDeps()
-  flattenDeps(json, deps)
+  const json = readProductionDeps('yarn list --prod --json 2>/dev/null')
+  flattenYarnDeps({ children: json.data.trees }, deps)
   return Object.keys(deps)
+}
+
+const useNpm = () => {
+  const deps = {}
+  const json = readProductionDeps('npm ls --prod --json 2>/dev/null')
+  flattenNpmDeps(json, deps)
+  return Object.keys(deps)
+}
+
+module.exports = () => {
+  const hasYarnLock = fs.existsSync(path.resolve('.', 'yarn.lock'))
+  return hasYarnLock ? useYarn() : useNpm()
 }
